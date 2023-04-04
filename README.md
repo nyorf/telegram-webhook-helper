673d19bbd6fcc623ee041b882a793a7ba7ce6ee7
Dmitry Dolgov <me@nyorf.com>
2023-04-04 18:31:05 +0700

    Initial release :)

---
+++ README.md
@@ -0,0 +1,21 @@
+# Telegram Bot API Webhook helper
+
+Hi! This is a simple, pure Python script that helps you check your Telegram Bot API webhook's status. 
+
+*Using only [requests](https://pypi.org/project/requests/) as an additional library.*
+
+## How to:
+Clone the repository: 
+
+    git clone https://github.com/nyorf/telegram-webhook-helper.git
+
+Set TOKEN and URL variables to your values in main.py.
+
+    .........
+    TOKEN = $YOUR_TOKEN_FROM_@BOTFATHER
+    URL = $URL_THAT_YOU_WANT_TO_SET_AS_WEBHOOK
+    .........
+
+Run script in CLI:
+
+    python3 main.py
---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+import json
+import time
+import requests
+from datetime import datetime
+
+#set these variables before running the script
+
+global TOKEN
+global URL
+#TOKEN = "your telegram token from @BotFather"
+#URL = "your webhook url"
+
+def menu():
+    print("webhook helper. reply with a choice (number)\n\n\
+1) set webhook to url from bot_credentials\n\
+2) delete current webhook\n\
+3) delete current webhook and drop pending updates\n\
+4) get webhook info")
+    user_choice = int(input("\nyour choice: "))
+    if user_choice == 1:
+        setWebhook()
+    elif user_choice == 2:
+        deleteWebhook()
+    elif user_choice == 3:
+        deleteWebhook(drop_pending_updates=True)
+    elif user_choice == 4:
+        getWebhookInfo()
+    else:
+        os.system("clear")
+        print("huh? try again.")
+        time.sleep(1.5)
+        os.system("clear")
+        menu()
+        
+
+def setWebhook():
+    set_webhook = requests.post(
+        'https://api.telegram.org/bot{token}/setWebhook'.format(
+            token = TOKEN
+        ),
+        headers={"Content-Type": "application/json"},
+        json={"url": URL}
+    )
+    response = set_webhook.json()
+    print("\nsetWebhook request sent. Response status code: {status_code}".format(status_code=set_webhook.status_code))
+    print("\nreadable setWebhook json response:\n\n", json.dumps(response, indent=4))
+
+
+def deleteWebhook(drop_pending_updates = False):
+    delete_webhook = requests.post(
+        'https://api.telegram.org/bot{token}/deleteWebhook'.format(
+            token = TOKEN
+        ),
+        headers={"Content-Type": "application/json"},
+        json={"drop_pending_updates": drop_pending_updates}
+    )
+    response = delete_webhook.json()
+    if not drop_pending_updates:
+        print("\ndeleteWebhook request sent. Response status code: {status_code}".format(status_code=delete_webhook.status_code))
+    elif drop_pending_updates:
+        print("\ndeleteWebhook request sent (with drop_pending_updates == True). \
+Response status code: {status_code}".format(status_code=delete_webhook.status_code))
+    print("\nreadable deleteWebhook json response:\n\n", json.dumps(response, indent=4))
+
+
+def getWebhookInfo():
+    get_webhook_info = requests.post(
+        'https://api.telegram.org/bot{token}/getWebhookInfo'.format(
+            token = TOKEN
+        ),
+        headers={"Content-Type": "application/json"}
+    )
+    response = get_webhook_info.json()
+    if response["result"]["url"] == '':
+        print("\nwebhook is not set.")
+    elif "last_error_date" in response["result"]:
+        readable_date = datetime.fromtimestamp(response["result"]["last_error_date"]).strftime("%Y-%m-%d %H:%M:%S")
+        timestamp = response["result"]["last_error_date"]
+        response["result"]["last_error_date"] = "{r_d} (timestamp: {ts})".format(
+            r_d = readable_date,
+            ts = timestamp
+        )
+    print("\nreadable getWebhookInfo json response:\n\n", json.dumps(response, indent=4))
+
+
+try:
+    menu()
+except KeyboardInterrupt:
+    print("\n\n\nchange da world, my final message. goodbye...")
+except NameError:
+    print("\nerror: please, specify your Telegram token and webhook url as global TOKEN and URL variables")
